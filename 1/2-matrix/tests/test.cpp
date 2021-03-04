184e753b379082b28ad9639620778559dc846e21
Elena Beleva <lenka.hakuna.matata@gmail.com>
2021-03-04 20:02:58 +0500

    add googletest

--- 1/2-matrix/tests/test.cpp
+++ 1/2-matrix/tests/test.cpp
@@ -1,17 +1,27 @@
-#include "../src/matrix.h"
-#include "../src/matrix_utils.h"
+// #include "../src/matrix.h"
+// #include "../src/matrix_utils.h"
 
-int main()
-{
-    // test case 1
-    {
-        Matrix A, B;
-        rand(A), rand(B);
-        print(A), print(B);
+// int main()
+// {
+//     // test case 1
+//     {
+//         Matrix A, B;
+//         rand(A), rand(B);
+//         print(A), print(B);
 
-        Matrix C = A * B;
-        print(C);
-    }
+//         Matrix C = A * B;
+//         print(C);
+//     }
 
-    return 0;
+//     return 0;
+// }
+
+// #include "matrix.h"
+// #include "matrix_utils.h"
+
+#include <gtest/gtest.h>
+
+TEST (Test, test) { 
+    EXPECT_EQ(0,0);
 }
+
